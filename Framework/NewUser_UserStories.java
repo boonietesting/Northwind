6bae8d292c1b086d24ecc543af0c4a7c529f7e03
BoonieTesting <BoonieTesting>
2023-02-27 18:20:03 +0000

    New Test Cases

--- Framework/NewUser_UserStories.java
+++ Framework/NewUser_UserStories.java
@@ -16,14 +16,14 @@ import io.restassured.RestAssured;
 
 //https://riley.partner.testinsights.io/app/#!/model-engine/guid/9d7639a6-524b-4e5b-aa58-2e8ecea4bcb8
 @Listeners({TestNGListener.class, ExtentTestListener.class})
-@TestModellerSuite(id = 198, profileId = 100281)
+@TestModellerSuite(id = 199, profileId = 100281)
 public class NewUser_UserStories extends TestBase
 {
     
 
     
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "464c0680-9a01-4e89-9606-eda92048659e")
+    @TestModellerPath(guid = "337622aa-9596-4954-9e44-8f59441813ad")
     public void NegativeSuccessful200()
     {
         
@@ -34,167 +34,189 @@ public class NewUser_UserStories extends TestBase
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "8556166f-e74d-40ab-87f5-f0b8e92104df")
+    @TestModellerPath(guid = "f7703a67-6f2f-4546-aad6-907ed1805df5")
     public void NegativeSuccessful2001()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("aaf33002-2edf-4609-8013-082166386229");
-    _Post_createUser.Post_createUser_200("Iliana_Kunze25","#!_ @","","ben","","",12345);
+    _Post_createUser.Post_createUser_200("Kevon_Johnston","#!_ @","","ben","","",12345);
 
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "b0284897-5ec4-4fa8-9182-c7e29a9489b2")
+    @TestModellerPath(guid = "c0f9d23c-4d29-44ec-90fa-2383d25b471f")
     public void NegativeSuccessful2002()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("aaf33002-2edf-4609-8013-082166386229");
-    _Post_createUser.Post_createUser_200("Jaylan26","","","ben","","",12345);
+    _Post_createUser.Post_createUser_200("Shawna.Grady","#!_ @","","ben","","#!_ @",12345);
 
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "5035e419-d40e-427e-b7a4-b5f07f7be822")
+    @TestModellerPath(guid = "25809666-13de-4f88-b6bb-99eb02c47c19")
     public void NegativeSuccessful2003()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("aaf33002-2edf-4609-8013-082166386229");
-    _Post_createUser.Post_createUser_200("Carlie30","Hassie","","ben","","",12345);
+    _Post_createUser.Post_createUser_200("Micaela80","","","ben","","",12345);
 
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "6bb562e1-9457-4212-94ae-f709861a901a")
-    public void NegativeSuccessful2004()
-    {
-        
-           pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
-    TestModellerLogger.SetLastNodeGuid("aaf33002-2edf-4609-8013-082166386229");
-    _Post_createUser.Post_createUser_200("Carlie85","Tiana","#!_ @","ben","","",12345);
-
-    }
-
-    @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "5f69919e-4c49-4817-aac9-63b6b75a02f5")
-    public void NegativeSuccessful2005()
-    {
-        
-           pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
-    TestModellerLogger.SetLastNodeGuid("aaf33002-2edf-4609-8013-082166386229");
-    _Post_createUser.Post_createUser_200("Ila59","Stephen","Turcotte","ben","4wyd7N5WZR","",12345);
-
-    }
-
-    @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "76a61085-a346-4343-b0db-3dc02c244a24")
-    public void NegativeSuccessful2006()
-    {
-        
-           pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
-    TestModellerLogger.SetLastNodeGuid("aaf33002-2edf-4609-8013-082166386229");
-    _Post_createUser.Post_createUser_200("Darrell_Kovacek22","Anabelle","Lehner","ben","4iIlS4DVeh","#!_ @",12345);
-
-    }
-
-    @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "79c8cecc-3a56-4c88-a4cd-4fa779c1b51f")
+    @TestModellerPath(guid = "d817855b-55b4-45a4-b32e-e07a35c84a43")
     public void PositiveSuccessful200NegativeSuccessful200()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("55b223d3-0995-4303-94e6-600954889deb");
-    _Post_createUser.Post_createUser_200("Craig.Bauch","Anika","Wiegand","ben","MoxgkwzGle","1-240-921-5625 x0341",12345);
+    _Post_createUser.Post_createUser_200("Edwardo_Ullrich43","Mariane","","ben","","(771) 655-1135",12345);
 
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "931330c7-f502-40b6-9b60-291704f97fe2")
+    @TestModellerPath(guid = "f845c799-aa3b-4126-88b7-e6d272cd305b")
     public void PositiveSuccessful200NegativeSuccessful2001()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("55b223d3-0995-4303-94e6-600954889deb");
-    _Post_createUser.Post_createUser_200("Haylee.Hauck28","Piper","Stanton","ben","6unSCmJjWM","747.600.4681",12345);
+    _Post_createUser.Post_createUser_200("Isaias_Johnson45","Andreane","","ben","","954-985-2832 x60621",12345);
 
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "4dbd0b44-4614-46da-8b0f-e2df95be9015")
+    @TestModellerPath(guid = "dbacc648-cb5d-4c7f-aa6c-726961258d1f")
     public void PositiveSuccessful200NegativeSuccessful2002()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("55b223d3-0995-4303-94e6-600954889deb");
-    _Post_createUser.Post_createUser_200("Hazel_Renner88","Julia","Boehm","ben","VrKR0gOTsg","543.286.0072",12345);
+    _Post_createUser.Post_createUser_200("Chaim.Jerde","Royal","","ben","","(228) 625-1510",12345);
 
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "c6e7b3e8-b068-4961-b417-01c2a77b89d7")
+    @TestModellerPath(guid = "df44ac3b-e482-48ec-ad9f-c757b1d23b17")
     public void PositiveSuccessful200PositiveSuccessful200()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("55b223d3-0995-4303-94e6-600954889deb");
-    _Post_createUser.Post_createUser_200("Ernie_King83","Gina","Brakus","ben","vDciAOKxUK","1-567-218-1081",12345);
+    _Post_createUser.Post_createUser_200("Julianne.Boyer10","Wayne","","ben","","481-688-2015 x6750",12345);
 
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "79eba249-fcef-4809-a7f7-11d6f3b6d255")
+    @TestModellerPath(guid = "1153ffa9-1c4d-4af5-9ac9-7cb3390a90c7")
     public void PositiveSuccessful200PositiveSuccessful2001()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("55b223d3-0995-4303-94e6-600954889deb");
-    _Post_createUser.Post_createUser_200("Vicenta_Mertz","Baby","Jacobs","ben","AbR4lbafPw","229.510.5649 x80205",12345);
+    _Post_createUser.Post_createUser_200("Vicenta.Gibson95","Sophie","","ben","","791.997.2105 x19032",12345);
 
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "dc6f3cee-997e-484e-9a8f-b4951cc50efa")
+    @TestModellerPath(guid = "eef42c0c-2e54-42ff-8fb5-704e166a9659")
     public void PositiveSuccessful200PositiveSuccessful2002()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("55b223d3-0995-4303-94e6-600954889deb");
-    _Post_createUser.Post_createUser_200("Enoch_Rau","Wilburn","Berge","ben","u6pRofngsE","1-610-683-1544",12345);
+    _Post_createUser.Post_createUser_200("Claudine39","Lisandro","","ben","","640.601.0538 x020",12345);
 
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "e3cb475c-8939-4dce-a72a-d3cada25aa06")
+    @TestModellerPath(guid = "ddd5e6e1-ff5d-4fd3-a180-dbf2ba5fc4dd")
     public void PositiveSuccessful200NegativeSuccessful2003()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("55b223d3-0995-4303-94e6-600954889deb");
-    _Post_createUser.Post_createUser_200("Eloy49","Jerald","Ledner","ben","jWWbLpkO2M","756-903-3556",12345);
+    _Post_createUser.Post_createUser_200("Magdalen.Bins5","Diego","","ben","","702-953-9100",12345);
 
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "1f409d2d-6350-4475-9853-d69317678b34")
+    @TestModellerPath(guid = "e640f63e-60bb-40e3-ad71-a1174d82e0e6")
     public void PositiveSuccessful200NegativeSuccessful2004()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("55b223d3-0995-4303-94e6-600954889deb");
-    _Post_createUser.Post_createUser_200("Calista80","Catherine","Sawayn","ben","oLlD35YTtC","573.440.9820",12345);
+    _Post_createUser.Post_createUser_200("Makayla_Pollich","Vilma","","ben","","642.511.3380 x5113",12345);
 
     }
 
     @Test  (groups= {"New User","New User - User Stories"})
-    @TestModellerPath(guid = "64b65177-48aa-4747-8c36-39767b6744bf")
+    @TestModellerPath(guid = "9e73a87b-6df6-47e9-98dc-d2bac00263c2")
+    public void NegativeSuccessful2004()
+    {
+        
+           pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
+    TestModellerLogger.SetLastNodeGuid("aaf33002-2edf-4609-8013-082166386229");
+    _Post_createUser.Post_createUser_200("Favian79","Nayeli","","ben","","",12345);
+
+    }
+
+    @Test  (groups= {"New User","New User - User Stories"})
+    @TestModellerPath(guid = "82cce234-6e57-4eb3-82e6-1f1d07430b57")
+    public void NegativeSuccessful2005()
+    {
+        
+           pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
+    TestModellerLogger.SetLastNodeGuid("aaf33002-2edf-4609-8013-082166386229");
+    _Post_createUser.Post_createUser_200("Kaelyn26","Justine","#!_ @","ben","","",12345);
+
+    }
+
+    @Test  (groups= {"New User","New User - User Stories"})
+    @TestModellerPath(guid = "881c1b37-e404-4801-a7b3-9e477b36e153")
+    public void NegativeSuccessful2006()
+    {
+        
+           pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
+    TestModellerLogger.SetLastNodeGuid("aaf33002-2edf-4609-8013-082166386229");
+    _Post_createUser.Post_createUser_200("Maxime.Price","Emiliano","Konopelski","ben","Bnp5woCY78","",12345);
+
+    }
+
+    @Test  (groups= {"New User","New User - User Stories"})
+    @TestModellerPath(guid = "97d3175e-c948-4d0c-b8a9-72d4a4ba445c")
     public void NegativeSuccessful2007()
     {
         
            pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("aaf33002-2edf-4609-8013-082166386229");
-    _Post_createUser.Post_createUser_200("Brando_Rau","Deron","Jones","ben","#!_ @","",12345);
+    _Post_createUser.Post_createUser_200("Peyton_Barrows77","Geraldine","Nitzsche","ben","ghoZxkSbDw","#!_ @",12345);
+
+    }
+
+    @Test  (groups= {"New User","New User - User Stories"})
+    @TestModellerPath(guid = "fa88cefa-a2f9-4032-86b1-86fed62bd4d7")
+    public void PositiveSuccessful200NegativeSuccessful2005()
+    {
+        
+           pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
+    TestModellerLogger.SetLastNodeGuid("55b223d3-0995-4303-94e6-600954889deb");
+    _Post_createUser.Post_createUser_200("Jolie_Vandervort","Vicente","Hilll","ben","aM9SSNeMWd","1-446-554-0357 x6233",12345);
+
+    }
+
+    @Test  (groups= {"New User","New User - User Stories"})
+    @TestModellerPath(guid = "542c6b6a-61f6-4591-81ce-8af262aab2ee")
+    public void NegativeSuccessful2008()
+    {
+        
+           pages.Post_createUser _Post_createUser = new pages.Post_createUser(getDriver());
+    TestModellerLogger.SetLastNodeGuid("aaf33002-2edf-4609-8013-082166386229");
+    _Post_createUser.Post_createUser_200("Shyann_Dare","Chyna","Schamberger","ben","#!_ @","",12345);
 
     }
 
