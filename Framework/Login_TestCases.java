5bc7cfa20ac7b98c25b284b8c20ef7499cb312c0
BoonieTesting <BoonieTesting>
2023-02-28 13:16:36 +0000

    Login-Auto-Ben

--- Framework/Login_TestCases.java
+++ Framework/Login_TestCases.java
@@ -16,36 +16,36 @@ import io.restassured.RestAssured;
 
 //https://riley.partner.testinsights.io/app/#!/model-engine/guid/4554562a-f214-4ebd-9e9c-b30879482483
 @Listeners({TestNGListener.class, ExtentTestListener.class})
-@TestModellerSuite(id = 226, profileId = 100314)
+@TestModellerSuite(id = 227, profileId = 100314)
 public class Login_TestCases extends TestBase
 {
     
 
     
     @Test  (groups= {"Login","Login - Test Cases"})
-    @TestModellerPath(guid = "573d7fdd-d9c7-4938-ae8a-42cbcafb6d47")
+    @TestModellerPath(guid = "7a5e1988-65cb-401b-9c4d-f817ba1c39c7")
     public void Positivesuccessfuloperation200()
     {
         
            pages.Get_loginUser _Get_loginUser = new pages.Get_loginUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("72bd5192-b1b0-4fa8-aaf2-e3a983c9abf9");
-    _Get_loginUser.Get_loginUser_200("Orville.Howe27","kWdFSlZ27e");
+    _Get_loginUser.Get_loginUser_200("Ben","EokrquOlWB");
 
     }
 
     @Test  (groups= {"Login","Login - Test Cases"})
-    @TestModellerPath(guid = "c1ba7c38-14c3-4a45-a670-837cd016f4b4")
+    @TestModellerPath(guid = "8e794a9a-d83d-4a6a-99a7-406b2f0264be")
     public void Negativesuccessfuloperation200()
     {
         
            pages.Get_loginUser _Get_loginUser = new pages.Get_loginUser(getDriver());
     TestModellerLogger.SetLastNodeGuid("f5a97c4d-03b4-4e45-8351-4c6d95cb94a2");
-    _Get_loginUser.Get_loginUser_200("Imogene83","#!_ @");
+    _Get_loginUser.Get_loginUser_200("Ben","#!_ @");
 
     }
 
     @Test  (groups= {"Login","Login - Test Cases"})
-    @TestModellerPath(guid = "44434378-3ca3-48f8-96d8-1304872ccd30")
+    @TestModellerPath(guid = "0b3ff099-7287-49b9-9539-89b492b55257")
     public void Negativesuccessfuloperation2001()
     {
         
